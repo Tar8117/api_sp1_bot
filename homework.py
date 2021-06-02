43cd684632b81b61000073ee45a7a2b9adcb49d8
Taron <tar8117@yandex.ru>
2021-06-03 00:32:27 +0300

    first

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 
@@ -11,11 +12,20 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        logging.error(f'неправильное значение: {homework_name}')
+        return 'неправильное значение'
+    status = homework['status']
+    if status not in ('approved', 'rejected'):
+        logging.error(f'неправильное значение: {status}')
+        return 'неправильное значение'
+    if status != 'approved':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -23,14 +33,17 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    params = {
+        'from_date': current_timestamp,
+        'PRACTICUM_TOKEN': PRAKTIKUM_TOKEN,
+    }
+    homework_statuses = requests.get(url=API_URL, headers=headers, params=params)
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
